4b446aec821cb22167836e4fdce17d0183b8093a
KANGSOONYONG <pliuhb@naver.com>
2021-10-05 17:37:53 +0900

    add : styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Youtuber from './component/Youtuber';
 
 function App() {
   return (
     <div className="App">
-      유튜버 할인 코드 사이트
+      <h1>유튜버 할인 코드 사이트</h1>
+        <p>리스트</p>
+        <Youtuber />
     </div>
   );
 }
---
+++ src/component/Youtuber.js
@@ -0,0 +1,10 @@
+export default function Youtuber(){
+    return (
+        <ul className="list">
+          <li>유튜버사진1</li>
+          <li>유튜버1</li>
+          <li>더 프로틴 웍스</li>
+          <li>미트리</li>
+        </ul>
+    )
+}
